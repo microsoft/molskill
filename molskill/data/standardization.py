6ea5cb6f886fab2ed80bc26422dd152282626160
Feng Tian <134106614+ftian9@users.noreply.github.com>
2026-06-24 09:21:58 -0700

    skip downloading the moments file if it already exists

--- molskill/data/standardization.py
+++ molskill/data/standardization.py
@@ -29,7 +29,7 @@ def get_population_moments(
         Dict[str, np.ndarray]: population {mean: np.ndarray, std: np.ndarray}
     """
 
-    if moment_csv is None:
+    if moment_csv is None and not os.path.exists(MOMENT_CSV):
         LOGGER.info("Standardization moments not found. Downloading from remote...")
         moment_csv = MOMENT_CSV
         os.makedirs(ASSET_PATH, exist_ok=True)
